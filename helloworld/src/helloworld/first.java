15736cab7fa41405a723c73477a75b7e5b7775c1
Joseph3214 <joeamm1993@gmail.com>
2022-09-20 13:14:30 -0700

    my first project

---
+++ helloworld/src/helloworld/first.java
@@ -0,0 +1,17 @@
+package helloworld;
+
+public class first {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		
+		int k = 25;
+		
+		for(int i = 0; i < 20; i++)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
